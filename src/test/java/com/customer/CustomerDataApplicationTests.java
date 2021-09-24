74fe787d026aa5dab1abaff06728213b643ffeb1
navyareddy-09 <89064186+navyareddy-09@users.noreply.github.com>
2021-09-24 22:01:53 +0530

    Update CustomerDataApplicationTests.java

--- src/test/java/com/customer/CustomerDataApplicationTests.java
+++ src/test/java/com/customer/CustomerDataApplicationTests.java
@@ -1,16 +1,1 @@
-package com.customer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.BeanCreationException;
-import javax.persistence.PersistenceException;
-
-
-@SpringBootTest
-class CustomerDataApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
